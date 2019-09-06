8a48776940d1178f29c7bdfd60e79c468691807b
chenleihckj01 <hckjchenlei01@126.com>
2019-09-06 17:43:10 +0800

    Initial commit

---
+++ src/demo1/HelloWorld.java
@@ -0,0 +1,12 @@
+package demo1;
+
+/**
+ * 2019/09/2019/9/6 九月
+ * Administrator
+ * project3
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
